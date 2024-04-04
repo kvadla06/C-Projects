6002dd774c96cad893d809cfb9044ca935ae752c
Kavya Rani Vadla <krvadla@engr-ras-208.eos.ncsu.edu>
2024-04-03 23:19:11 -0400

    passed tests

--- p5/hash.c
+++ p5/hash.c
@@ -6,8 +6,41 @@
 
 #include "sha256.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-int main() {
-    
-    
+int main(int argc, char *argv[]) {
+    FILE *file = stdin;
+    SHAState *state = makeState();
+    byte buffer[BLOCK_SIZE];
+    int bytes_read;
+    if (argc > 2) {
+        fprintf( stderr, "usage: hash [input_file]\n");
+        exit( EXIT_FAILURE );
+    }
+    if (argc > 1) {
+        file = fopen(argv[1], "rb");
+        if (!file) {
+            fprintf(stderr, "%s: No such file or directory\n", argv[1]);
+            return 1;
+        }
+    }
+
+    while ((bytes_read = fread(buffer, 1, BLOCK_SIZE, file)) > 0) {
+        update(state, buffer, bytes_read);
+    }
+
+    word hash[HASH_WORDS];
+    digest(state, hash);
+
+    for (int i = 0; i < HASH_WORDS; i++) {
+        printf("%08x", hash[i]);
+    }
+    printf("\n");
+
+    freeState(state);
+    if (file != stdin) {
+        fclose(file);
+    }
+
+    return 0;
 }
